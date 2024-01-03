0a2d639f81522b8ed383b47497f1993959b589b0
Niovan9 <niovan.martinez@uplight.com>
2024-01-03 02:37:34 -0400

    Add stub file for geo_territories.utils.py

--- innovatix/geo_territories/utils.py
+++ innovatix/geo_territories/utils.py
@@ -7,21 +7,21 @@ from innovatix.users.constants import DEFAULT_COUNTRY_CODE, DEFAULT_PROVINCE_COD
 logger = logging.getLogger("django")
 
 
-def create_country(**kwargs: Any) -> Country:
+def create_country(**kwargs: dict[str, Any]) -> Country:
     model_params = {}
     model_params.update(**kwargs)
 
     return Country.objects.create(**model_params)
 
 
-def create_province(country: Country, **kwargs: Any):
+def create_province(country: Country, **kwargs: dict[str, Any]) -> Province:
     model_params = {"country": country}
     model_params.update(**kwargs)
 
     return Province.objects.create(**model_params)
 
 
-def get_default_country():
+def get_default_country() -> Country:
     try:
         return Country.objects.get(code=DEFAULT_COUNTRY_CODE)
     except Country.DoesNotExist as err:
@@ -29,7 +29,7 @@ def get_default_country():
         raise Country.DoesNotExist
 
 
-def get_default_province():
+def get_default_province() -> Province:
     try:
         return Province.objects.get(
             code=DEFAULT_PROVINCE_CODE, country=get_default_country()
