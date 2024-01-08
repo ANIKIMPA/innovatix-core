0507a131795b088cdcf1cb86bdd5fb018a1d86ed
Niovan9 <niovan.martinez@uplight.com>
2024-01-08 02:27:41 -0400

    Include fixtures when installing package

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name="innovatix-core",
-    version="0.2.4",
+    version="0.2.5",
     install_requires=[
         "Django>=4.2.3",
         "django-summernote>=0.8.20.0",
@@ -10,4 +10,8 @@ setup(
         "python-dateutil>=2.8.2",
         "stripe>=5.5.0",
     ],
+    packages=find_packages(),
+    package_data={
+        "innovatix.core": ["fixtures/*.json"],
+    },
 )
