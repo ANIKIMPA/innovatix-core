dd049ec87d65bc2ffc01595d8b0b94500bae44d6
Niovan9 <niovan.martinez@uplight.com>
2024-02-09 00:44:13 -0400

    Remove webhook view from products urls

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-
 from products.views import (
     CustomerInfoFormView,
     MembershipDetailView,
     MembershipListView,
-    webhook_received,
 )
 
 app_name = "products"
@@ -19,10 +17,5 @@ urlpatterns = [
         "membresias/<slug:slug>/usuario-info/",
         CustomerInfoFormView.as_view(),
         name="customer-info",
-    ),
-    path(
-        "webhooks/stripe/",
-        webhook_received,
-        name="stripe-webhook",
     ),
 ]
