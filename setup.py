81f61c0cae94bf6b3d79b3a7ffb30978c29761bc
Niovan9 <niovan.martinez@uplight.com>
2024-04-07 23:37:10 -0400

    Update dependencies

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import find_packages, setup
 
 setup(
     name="innovatix-core",
-    version="0.3.0",
+    version="0.3.1",
     install_requires=[
         "Django>=4.2.3",
         "django-summernote>=0.8.20.0",
         "phonenumbers>=8.13.17",
         "python-dateutil>=2.8.2",
         "stripe>=5.5.0",
+        "django-allauth>=0.61.1",
     ],
     packages=find_packages(),
     package_data={
